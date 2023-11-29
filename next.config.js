e591b2a40b2876bc976baf91b8dd66ac33104e6b
jbh105 <jaydipbhayani105@gmail.com>
2023-11-29 14:39:51 +0530

    update PWA

--- next.config.js
+++ next.config.js
@@ -1,21 +1,23 @@
 require("dotenv").config();
 
-const withPWA = require("next-pwa")({
+const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
-  register: true,
-  skipWaiting: true,
-  disableDevLogs: true,
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: process.env.NODE_ENV === "development",
+  workboxOptions: {
+    disableDevLogs: true,
+  },
 });
 
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    BASE_URL: process.env.BASE_URL,
+  },
+};
 
-const nextConfig = withBundleAnalyzer(
-  withPWA({
-    reactStrictMode: true,
-    env: {
-      BASE_URL: process.env.BASE_URL,
-    },}))
-
-    module.exports = nextConfig
+module.exports = withPWA(nextConfig);
